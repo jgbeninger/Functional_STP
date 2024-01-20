384531edd74b0d99c2072da378a50ba23e92339c
jgbeninger <jgbeninger@yahoo.ca>
2024-01-20 16:50:10 -0500

    Update generate_fig4.sh (#10)

--- generate_fig4.sh
+++ generate_fig4.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
-#This script runs all the code necessary to generate the plots for figure 3
-mkdir fig3
+#This script runs all the code necessary to generate the plots for figure 4
+mkdir fig4
+mkdir fig3 #created folder for fig3 because the similarity_measures script creates plots for both fig3 and fig4
 python process_files_rodent.py srp_fits_rodent_1.3mM/*
 python process_files_human.py srp_fits_human/*
-mkdir fig2 #create a folder for fig2 because the Silhouette plots script generates figure for both fig2 and fig3
-python silhouette_plots.py
-python Human_Single_kernel_plots.py srp_fits_human/*
-python joint_pc_projections.py
-python human_plots.py srp_fits_human/*
-mkdir fig4 #create a folder for fig4 because the similarity measures script generates plots for both fig3 and fig4
 python similarity_measures.py
+
+mkdir ./fig4/dists
+python bootstrap_fig4b.py srp_fits_rodent_1.3mM/*
+python bootstrap_diff1.3mMdownsampledmlp.py srp_fits_rodent_1.3mM/*
+python bootstrap_diff2mMmlp.py srp_fits_rodent_2mM/*
+python downsample_comp.py
